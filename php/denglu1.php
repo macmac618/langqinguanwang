86cadfcd92fe7729b0dcb1de6e889d07fe1d30de
mac <1412518057@qq.com>
2019-10-16 12:11:28 +0800

    腕表比较在做

---
+++ php/denglu1.php
@@ -0,0 +1,24 @@
+<?php
+     header("Content-type:text/html;charset=utf-8");
+     //1、接收前端的数据
+     $phone = $_POST['phon'];
+     $userpass = $_POST['passw'];
+
+     //处理数据
+     $conn = mysql_connect("localhost","root","root");
+     if(!$conn){
+          echo("数据库出错");
+     }else{
+          mysql_select_db("langqin",$conn);
+          $sqlstr = "select * from langqinguanwang where phone = '$phone'";
+          $result = mysql_query($sqlstr,$conn);
+          $rows = mysql_num_rows($result);
+          if($rows>0){
+               mysql_close($conn);
+               echo "1";//登录成功
+          }else{
+               
+             echo "0";//登录失败
+          }
+     }
+?>
